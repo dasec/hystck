2ef1dc5af8307cd8b7c04b6af3158317655fec05
Martin Thißen <martin.thissen@stud.h-da.de>
2019-11-28 10:53:02 +0000

    Update mail_attachement.py

--- scenarios/mail_attachement.py
+++ scenarios/mail_attachement.py
@@ -16,8 +16,9 @@ client = vmm.create_guest(guest_name='mail_attachement_vm')
 # path to the nfs mount point
 nfs_path = "/data/hystck_data/"
 filename = "attachement.txt"
+path = "C:\Users\hystck\Documents\document.txt"
 
-path_to_file = os.path(nfs_path, filename)
+path_to_file = os.path(path)
 
 # Wait for the VM to become ready and connect to the VMM
 client.waitTillAgentIsConnected()
@@ -46,7 +47,7 @@ while mail.is_busy:
 time.sleep(10)
 
 # Send a new mail by invoking thunderbird with special command line options
-mail.send_mail(message="testmail", subject="testmail", receiver="thomas.schaefer91@gmx.de", attachement=path_to_file)
+mail.send_mail(message="testmail", subject="testmail", receiver="martin-thissen97@web.de", attachement=path_to_file)
 while mail.is_busy:
     time.sleep(1)
 time.sleep(10)
