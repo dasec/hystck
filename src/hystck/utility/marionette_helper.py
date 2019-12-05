8d00560a127b38eb2199804766cbfb005377e145
thomas <thomas.a.schaefer@h-da.de>
2019-12-05 10:01:04 +0100

    added new testing output for thread exception.

--- src/hystck/utility/marionette_helper.py
+++ src/hystck/utility/marionette_helper.py
@@ -59,9 +59,14 @@ class MarionetteHelper:
         #self.logger.debug("Closing firefox")
         #self.client._send_message("Marionette:Quit")
         self.client._request_in_app_shutdown("eForceQuit")
-        self.client.delete_session(False)
-        self.client.cleanup()
-        self.client = None # reset client state
+        try:
+            self.client.close()
+            self.client.delete_session(False)
+            self.client.cleanup()
+        except Exception as e:
+            pass
+        finally:
+            self.client = None # reset client state
         #try:
         #    self.client.close()  # try to close the window anyway
         #except InvalidSessionIdException:
