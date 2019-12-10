f07b24c3adbefd5b0b5226898c72f0a8ee1c38fe
thomas <thomas.a.schaefer@h-da.de>
2019-12-10 11:22:17 +0100

    changed testrun2 to handle command line parameters to specify which modules should be tested.

--- examples/testrun2.py
+++ examples/testrun2.py
@@ -22,8 +22,8 @@ def create_vm(logger):
     guests = []
     guestListener = GuestListener(guests, logger)
     virtual_machine_monitor1 = Vmm(macsInUse, guests, logger)
-    guest = virtual_machine_monitor1.create_guest(guest_name="w-guest01", platform="windows", boottime=None)
-    # guest = virtual_machine_monitor1.create_guest(guest_name="w-guest01", platform="windows", boottime=None)
+    guest = virtual_machine_monitor1.create_guest(guest_name="l-guest01", platform="linux", boottime=None)
+    #guest = virtual_machine_monitor1.create_guest(guest_name="w-guest01", platform="windows", boottime=None)
     logger.debug("Try connecting to guest")
 
     while guest.state != "connected":
@@ -47,13 +47,15 @@ def main():
 
         guest = create_vm(logger)
 
-        browser_obj = None
-        browser_obj = guest.application("webBrowserFirefox", {'webBrowser': "firefox"})
-        browser_obj.open(url="faz.net")
-        time.sleep(30)
-        browser_obj.browse_to("heise.de")
-        time.sleep(20)
-        browser_obj.close()
+        # call all test functions you want to run
+        tests = {'firefox': test_firefox, 'thunderbird': test_thunderbird} # all available test functions
+        arguments = sys.argv[1:] # every argument but the first as the first is the filename
+
+        for argument in arguments:
+            try:
+                tests[argument](guest, logger)
+            except Exception as e:
+                print("argument unknown: " + str(e))
 
         time.sleep(5)
         guest.remove("keep")
@@ -85,6 +87,61 @@ def main():
             logger.debug("well, host1 was not defined!")
         sys.exit(1)
 
+
+def test_firefox(guest, logger):
+    logger.debug("test_firefox() started.")
+    browser_obj = None
+    browser_obj = guest.application("webBrowserFirefox", {'webBrowser': "firefox"})
+    browser_obj.open(url="faz.net")
+    while browser_obj.is_busy:
+        time.sleep(2)
+    browser_obj.browse_to("heise.de")
+    while browser_obj.is_busy:
+        time.sleep(5)
+    time.sleep(10)
+    browser_obj.close()
+    while browser_obj.is_busy:
+        time.sleep(5)
+    logger.debug("test_firefox() ended.")
+
+def test_thunderbird(guest, logger):
+    logger.debug("test_thunderbird() started.")
+    mail = guest.application("mailClientThunderbird", {})
+
+    mail.open()
+    while mail.is_busy:
+        time.sleep(2)
+    mail.close()
+    while mail.is_busy:
+        time.sleep(2)
+
+    time.sleep(20)
+
+    # Important set a password used by the mail service, it will be saved inside thunderbird
+    mail.set_session_password("newPass2019")
+    while mail.is_busy:
+        time.sleep(1)
+    # Prepare a new Profile; assume the profile folders don't exist; these options assume a insecure mail server without SSL/TLS using an unencrypted password exchange
+    # theo.11111@web.de / hystckMail / Theo Tester
+    mail.add_imap_account("imap.web.de", "smtp.web.de", "theo.test1@web.de", "theo.test1", "Theo Tester", "Example", 2,
+                          3, 1, 3)
+    while mail.is_busy:
+        time.sleep(1)
+    # Open thunderbird and check for mail
+    mail.open()
+    while mail.is_busy:
+        time.sleep(1)
+    # We are done close the application
+    mail.close()
+    while mail.is_busy:
+        time.sleep(1)
+    time.sleep(10)
+    # Send a new mail by invoking thunderbird with special command line options
+    mail.send_mail(message="testmail", subject="testmail", receiver="thomas.schaefer91@gmx.de")
+    while mail.is_busy:
+        time.sleep(1)
+    time.sleep(20)
+    logger.debug("test_thunderbird() ended.")
 
 if __name__ == "__main__":
     try:
