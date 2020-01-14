ebf1d67aa6c42c7cdfa7127c5e1308f3c94191de
AlexanderRohde <rohde.alx@gmail.com>
2020-01-14 16:43:04 +0100

    custom pcap file location for sniffer

--- src/hystck/core/sniffer.py
+++ src/hystck/core/sniffer.py
@@ -9,34 +9,22 @@ from hystck.utility.logger_helper import create_logger
 
 
 class Sniffer:
-    def __init__(self, name, hypervisor_ip, hypervisor_userAtHost,
-                 path='/usr/sbin/tcpdump',
-                 logger=None):
+    def __init__(self, name, path='/usr/sbin/tcpdump', pcap_path=os.getcwd(), logger=None):
         self.name = name
-        self.hypervisor_ip = hypervisor_ip
-        self.hypervisor_userAtHost = hypervisor_userAtHost
         self.path = path
         self.process = None
+        self.pcap_path = pcap_path
+        self.logger = logger
         if logger is None:
             self.logger = create_logger(name, logging.INFO)
-        else:
-            self.logger = logger
 
     def start(self):
         try:
-            network_dump_hypervisor_path = join(constants.FILEPATH_NETWORK_DUMPS, self.hypervisor_ip)
-            network_dump_guest_path = join(network_dump_hypervisor_path, self.name)
-            network_dump_file_path = join(network_dump_guest_path, str(int(time.time())) + ".pcap")
-
-            # setup dump directory structure
-            if not os.path.exists(network_dump_hypervisor_path):
-                os.mkdir(network_dump_hypervisor_path)
-
-            if not os.path.exists(network_dump_guest_path):
-                os.mkdir(network_dump_guest_path)
-
-            self.process = subprocess.Popen(
-                [self.path, "-i", constants.NETWORK_INTERNET_BRIDGE_INTERFACE, "-w", network_dump_file_path, "-s0"])
+            if not os.path.exists(self.pcap_path):
+                os.makedirs(self.pcap_path)
+            file = join(self.pcap_path, self.name + "_" + str(int(time.time())) + ".pcap")
+            cmd = [self.path, "-i", constants.NETWORK_INTERNET_BRIDGE_INTERFACE, "-w", file, "-s0"]
+            self.process = subprocess.Popen(cmd)
             self.logger.info("sniffer {} started".format(self.name))
         except Exception as e:
             self.logger.error("sniffer {} failed: ".format(self.name, str(e)))
