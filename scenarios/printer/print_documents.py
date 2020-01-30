b18012525d3f774950b56e3dad7eb20cfac000a1
Clemens Brockschmidt <clemens.brockschmidt@gmail.com>
2020-01-30 15:54:36 +0100

    Allow printing documents via cmd on Windows 10

--- scenarios/printer/print_documents.py
+++ scenarios/printer/print_documents.py
@@ -18,9 +18,16 @@ windows.waitTillAgentIsConnected()
 # Add printer
 windows.shellExec('rundll32 printui.dll,PrintUIEntry /if /b IPPTool-Printer /m "Generic / Text Only" /r "http://192.168.103.123:631/ipp/print/name"')
 time.sleep(3)
-windows.shellExec('rundll32 printui.dll,PrintUIEntry /y /n "IPPTool-Printer"')
+windows.shellExec('rundll32 printui.dll,PrintUIEntry /y /n IPPTool-Printer')
 time.sleep(3)
 windows.shellExec('REG ADD "HKCU\Software\Microsoft\Windows NT\CurrentVersion\Windows" /t REG_DWORD /v LegacyDefaultPrinterMode /d 1 /f')
-time.sleep(3000)
+time.sleep(5)
+
+# Print document
+
+#windows.runElevated('powershell -command "Get-Content -Path ./hystck-mpse_ws1920/packet_requirements.txt | Out-Printer"')
+time.sleep(5)
+
+windows.shellExec('notepad.exe /p "C:\Users\hystck\hystck\packet_requirements.txt')
 
 # windows.remove()
