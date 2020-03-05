73eac584b4d5d6f3506a77a1db4a4c1c631402c1
thomas <thomas.a.schaefer@h-da.de>
2020-03-05 12:21:44 +0100

    added first draft for windows installation script.

--- install_tools/windows_installation.bat
+++ install_tools/windows_installation.bat
@@ -1,5 +1,16 @@
 @echo off
-goto check_Permissions
+goto :check_Permissions
+goto :check_Prerequisites check
+
+IF %check% DO (
+    REM call install functions
+    python %~dp0pre_setup.py
+)
+
+echo "Installation finished"
+pause > nul
+exit
+goto :eof
 
 :check_Permissions
     echo Administrative permissions required. Detecting permissions...
@@ -12,3 +23,22 @@ goto check_Permissions
     )
 
     pause >nul
+
+:check_Prerequisites
+    FOR /F "tokens=2*" %%A IN (REG Query HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall /s /v DisplayName 2^>nul ^| find /I "for python 2.7"') DO (
+        set vcc=OK
+    )
+
+    FOR /F "tokens=2*" %%A IN (REG Query HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall /s /v DisplayName 2^>nul ^| find /I "python 2.7."') DO (
+        set pyt=OK
+    )
+
+    IF NOT %pyt%==OK echo "Python is not installed".&GOTO:eof
+    IF NOT %vcc%==OK echo "Visual C++ for Python is not installed".&GOTO:eof
+
+
+    REM return true
+    set %1=True
+
+
+cmd /k
