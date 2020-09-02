c6236c507a01c7f3dd9a538c19d1bb925f32e571
hystck <jan.tuerr@h-da.de>
2020-09-02 13:20:39 +0200

    updated linux installation; script now no longer needs to be executed as sudo

--- install_tools/linux_installation.sh
+++ install_tools/linux_installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#if [[ $EUID -ne 0 ]]; then
+   #echo "This script must be run as root" 1>&2
+  # exit 1
+#fi
 
 echo "Please choose if this installation is host (h) or guest (g) side installation:"
 read -p 'Selection: ' sel
@@ -23,12 +23,14 @@ fi
 echo "[~] Thank you. Installation will proceed for $param."
 
 # install python
-sh -c "apt-get install python -y -qqq"
+sh -c "sudo apt-get install python -y -qqq"
 
 echo "  [+] python installed."
 
 # call pre_setup.py
-sh -c "python pre_setup.py $param"
+sh -c "sudo python pre_setup.py $param"
+
+sh -c "python setup.py install --user"
 
 echo "  [+] pre_setup finished successfully."
 
